1b51af690258470bb313fa3c5dbfa14656ed1b61
Jackson Moore <jacksonmoore@tuta.io>
2018-08-16 17:48:30 +0200

    page title, header text, color

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -15,7 +15,7 @@ const TopBar = (props) => {
     <AppBar position="absolute" style={{zIndex:10}}>
       <Toolbar>
         <Typography variant="title" color="inherit" style={{flex: 1}}>
-          Multiplayer Tic Tac Toe
+          No Man's Land
         </Typography>
         {
           user &&
