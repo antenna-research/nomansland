8eef95300b939855d2a8a447a34747bc8befab16
Jackson Moore <jacksonmoore@tuta.io>
2018-08-16 09:55:33 +0200

    tweak board styling

--- client/src/components/games/Board.js
+++ client/src/components/games/Board.js
@@ -16,19 +16,27 @@ const renderCel = (makeMove, rowIndex, cellIndex, symbol, hasTurn) => {
       key={`${rowIndex}-${cellIndex}`}
     >·</button>
     { symbol == '2' && 
-    <svg viewbox="0 0 25 50">
+    <svg viewbox="0 0 25 50" className={'player2'}>
       <circle cx="8" cy="6" r="5" />
       <path d="M8,10 L8,20 L3,30 M8,20 L13,30 M0,15 L15,15"></path>
     </svg>
     }
     { symbol == '1' && 
-    <svg viewbox="0 0 25 50">
+    <svg viewbox="0 0 25 50" className={'player1'}>
       <circle cx="8" cy="6" r="5" />
       <path d="M8,10 L8,20 L3,30 M8,20 L13,30 M0,15 L15,15"></path>
     </svg>
     }
   </span>)
 }
+
+// <defs>
+//   <filter id="filter1" x="0" y="0">
+//     <feOffset result="offOut" in="SourceAlpha" dx="-5" dy="-5" />
+//     <feGaussianBlur result="blurOut" in="offOut" stdDeviation="3" />
+//     <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+//   </filter>
+// </defs>
 
 export default ({board, makeMove}) => board.map((cells, rowIndex) =>
   <div key={rowIndex}>
